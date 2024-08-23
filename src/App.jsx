ba7b49a034c404ea23a75c9d1683040801100075
karoolina1234 <karoolina.sm@gmail.com>
2024-08-23 17:01:14 -0300

    Adicionando e implementando a navbar do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <div className="container">
         <Outlet />
       </div>
